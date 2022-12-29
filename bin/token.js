b7a9f08c26f1b0a2d34c22789fdc3b079e9ac48d
richiemccoll <richie.mccoll@hotmail.co.uk>
2022-12-29 11:44:04 +0200

    feat: standard tooling project setup

--- bin/token.js
+++ bin/token.js
@@ -1,61 +1,61 @@
 const TokenType = {
-  LEFT_PAREN: "(",
-  RIGHT_PAREN: ")",
-  LEFT_BRACE: "{",
-  RIGHT_BRACE: "}",
-  COMMA: ",",
-  DOT: ".",
-  MINUS: "-",
-  PLUS: "+",
-  SEMICOLON: ";",
-  SLASH: "/",
-  STAR: "*",
+  LEFT_PAREN: '(',
+  RIGHT_PAREN: ')',
+  LEFT_BRACE: '{',
+  RIGHT_BRACE: '}',
+  COMMA: ',',
+  DOT: '.',
+  MINUS: '-',
+  PLUS: '+',
+  SEMICOLON: ';',
+  SLASH: '/',
+  STAR: '*',
 
-  BANG: "!",
-  BANG_EQUAL: "!=",
-  EQUAL: "=",
-  EQUAL_EQUAL: "==",
-  GREATER: ">",
-  GREATER_EQUAL: ">=",
-  LESS: "<",
-  LESS_EQUAL: "<=",
+  BANG: '!',
+  BANG_EQUAL: '!=',
+  EQUAL: '=',
+  EQUAL_EQUAL: '==',
+  GREATER: '>',
+  GREATER_EQUAL: '>=',
+  LESS: '<',
+  LESS_EQUAL: '<=',
 
-  IDENTIFIER: "id",
-  STRING: "str",
-  NUMBER: "num",
+  IDENTIFIER: 'id',
+  STRING: 'str',
+  NUMBER: 'num',
 
-  AND: "and",
-  CLASS: "class",
-  ELSE: "else",
-  FALSE: "false",
-  FUN: "fun",
-  FOR: "for",
-  IF: "if",
-  NIL: "nil",
-  OR: "or",
+  AND: 'and',
+  CLASS: 'class',
+  ELSE: 'else',
+  FALSE: 'false',
+  FUN: 'fun',
+  FOR: 'for',
+  IF: 'if',
+  NIL: 'nil',
+  OR: 'or',
 
-  PRINT: "print",
-  RETURN: "return",
-  SUPER: "super",
-  THIS: "this",
-  TRUE: "true",
-  VAR: "var",
-  WHILE: "while",
+  PRINT: 'print',
+  RETURN: 'return',
+  SUPER: 'super',
+  THIS: 'this',
+  TRUE: 'true',
+  VAR: 'var',
+  WHILE: 'while',
 
-  EOF: "eof",
-};
+  EOF: 'eof'
+}
 
 class Token {
   constructor(type, lexeme, literal, line) {
-    this.type = type;
-    this.lexeme = lexeme;
-    this.literal = literal;
-    this.line = line;
+    this.type = type
+    this.lexeme = lexeme
+    this.literal = literal
+    this.line = line
   }
 
-  toString(){
-    return `${this.type} ${this.lexeme} ${this.literal}`;
-  };
+  toString() {
+    return `${this.type} ${this.lexeme} ${this.literal}`
+  }
 }
 
-export {Token, TokenType}
+export { Token, TokenType }
